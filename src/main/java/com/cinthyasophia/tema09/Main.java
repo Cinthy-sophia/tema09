85731f7f4e37f8ff5b1e98f0dfb78561f9083076
cinthya <cinthysophia@gmail.com>
2019-02-04 17:39:43 +0100

    lo

--- src/main/java/com/cinthyasophia/tema09/Main.java
+++ src/main/java/com/cinthyasophia/tema09/Main.java
@@ -1,8 +1,10 @@
 package com.cinthyasophia.tema09;
 
+import com.cinthyasophia.tema09.ejercicio06.Ejercicio06;
+
 public class Main {
     public static void main(String[] args){
-
+        Ejercicio06 ejercicio06 = new Ejercicio06();
 
     }
 }
--- src/main/java/com/cinthyasophia/tema09/ejercicio06/Ejercicio06.java
+++ src/main/java/com/cinthyasophia/tema09/ejercicio06/Ejercicio06.java
@@ -1,5 +1,41 @@
 package com.cinthyasophia.tema09.ejercicio06;
 
 public class Ejercicio06 {
+    public Ejercicio06(){
+        int numero=2;
+        int[] vector= {-2,-1,0,1,2};
+
+        dividirEntreArray(numero,vector);
+
+
+    }
+    public void dividirEntreArray(int num, int[] vector){
+        double resultado;
+        int puntero=0;
+        boolean isFull;
+        do {
+            isFull=false;
+            try{
+                if (puntero==vector.length){
+                    isFull=true;
+                }
+                if (vector[puntero]!=0){
+                    resultado=num/vector[puntero];
+                    System.out.println(num+" / "+vector[puntero]+" = "+resultado);
+                    puntero++;
+
+                }else{
+                    System.out.println("Una division entre cero no es posible.");
+
+                }
+
+            }catch (ArithmeticException ae){
+                System.out.println("Una division entre cero no es posible.");
+                puntero++;
+            }
+
+        }while(!isFull);
+
+    }
 
 }
