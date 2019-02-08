f6e0401c8b2a247c1b3faeac9f7177f618633051
cinthya <cinthysophia@gmail.com>
2019-02-08 13:57:10 +0100

    Añado ejercicio 10 a medio hacer

--- src/main/java/com/cinthyasophia/tema09/ejercicio10/Calculadora.java
+++ src/main/java/com/cinthyasophia/tema09/ejercicio10/Calculadora.java
@@ -1,54 +1,44 @@
 package com.cinthyasophia.tema09.ejercicio10;
 
 import java.util.InputMismatchException;
-import java.util.Scanner;
+import java.util.Random;
 
 /**
  * Calculadora
  */
 
 public class Calculadora {
-    public Scanner lector = new Scanner (System.in);
+    public Random rnd = new Random();
     public boolean isNumber;
+    public final int SLEEP=1000;
 
-    public Calculadora() {
+    public Calculadora() throws InterruptedException {
 
         int decision;
         do {     
            decision= menuCalculadora();
-            //Utilizo un switch para cada caso.
+           Thread.sleep(SLEEP);
+
             switch (decision) {
 
-                //En caso de suma.
                 case 1:
                    suma();
                     break;
-
-                //En caso de resta.
 
                 case 2:
                     resta();
                     break;
 
-                //En caso de multiplicacion.
-
                 case 3:
                     multiplicacion();
-                        
                     break;
-
-                //En caso de division.
 
                 case 4:
                     division();
-                    
                     break;
-
-                //En caso de que pidan el resto de una division.
 
                 case 5:
                     restoDivision();
-                    
                     break;
 
                 default: 
@@ -56,7 +46,6 @@ public class Calculadora {
                     break;
             }
 
-           //El bucle dejara de ejecutarse cuando el usuario introduzca un 0. 
         } while (decision != 0);
 
 
@@ -65,119 +54,49 @@ public class Calculadora {
         float pSumando=0;
         float sSumando=0;
         float suma;
-        do {
-            System.out.print("Primer sumando: ");
-            try{
-                pSumando= lector.nextFloat();
-                isNumber=true;
-
-            }catch (InputMismatchException ime){
-                System.out.println("Debes introducir un numero no una letra, vuelve a intentarlo.");
-                isNumber=false;
-
-            }finally {
-                lector.nextLine();
-            }
-        }while(!isNumber);
-
-
-        do {
-            System.out.print("Segundo sumando: ");
-            try{
-                sSumando= lector.nextFloat();
-                isNumber=true;
-
-            }catch (InputMismatchException ime){
-                System.out.println("Debes introducir un numero no una letra, vuelve a intentarlo.");
-                isNumber=false;
-
-            }finally {
-                lector.nextLine();
-            }
-        }while(!isNumber);
 
         suma= pSumando + sSumando;
 
         System.out.println(pSumando +" + " +sSumando+ " = "+ suma);
     }
     public void resta(){
-        float minuendo;
-        float sustraendo;
+        float minuendo=0;
+        float sustraendo=0;
         float resta;
-
-        System.out.print("Sustraendo: ");
-        sustraendo= lector.nextFloat();
-        lector.nextLine();
-
-        System.out.print("Minuendo: ");
-        minuendo= lector.nextFloat();
-        lector.nextLine();
-
 
         resta= sustraendo - minuendo;
 
         System.out.println(sustraendo+ " - "+minuendo+ " = "+ resta);
     }
     public void multiplicacion(){
-        float pFactor;
-        float sFactor;
+        float pFactor=0;
+        float sFactor=0;
         float producto;
-
-        System.out.print("Introduce los datos y presiona intro: ");
-        pFactor= lector.nextFloat();
-        lector.nextLine();
-
-        sFactor= lector.nextFloat();
-        lector.nextLine();
-
-        // Se ejecuta la multiplicacion y luego se imprime.
 
         producto= pFactor * sFactor;
 
         System.out.println(pFactor +" * "+ sFactor +  " = "+ producto);
     }
     public void division(){
-        float dividendo;
-        float divisor;
+        float dividendo=0;
+        float divisor=0;
         float cociente;
-
-        System.out.print("Introduce los datos y presiona intro: ");
-        dividendo= lector.nextFloat();
-        lector.nextLine();
-
-        divisor= lector.nextFloat();
-        lector.nextLine();
-
-        // Se ejecuta la division y luego se imprime.
 
         cociente= dividendo / divisor;
 
         System.out.println(dividendo +" / "+ divisor + " = "+ cociente);
     }
     public void restoDivision(){
-        float rDividendo;
-        float rDivisor;
+        float rDividendo=0;
+        float rDivisor=0;
         float resto;
-
-        System.out.print("Introduce los datos y presiona intro: ");
-        rDividendo= lector.nextFloat();
-        lector.nextLine();
-
-
-        rDivisor= lector.nextFloat();
-        lector.nextLine();
-
-        // Se ejecuta la division, se calcula el resto y luego se imprime.
 
         resto= rDividendo % rDivisor;
 
         System.out.println("El resto de "+rDividendo +" / "+ rDivisor + " es "+ resto);
     }
     public int menuCalculadora() {
-        int decision=0;
-        boolean isNumber;
-        //La lista de ordenes a ejecutar.
-        System.out.println("Elige una opcion y presiona intro:");
+        int decision;
         System.out.println("1. Suma");
         System.out.println("2. Resta");
         System.out.println("3. Multiplicacion");
@@ -186,23 +105,27 @@ public class Calculadora {
         System.out.println("-------------------------");
         System.out.println("0. Salir");
         do {
-            try{
-                decision = lector.nextInt();
-                isNumber=true;
+            decision=datosAleatorios();
 
-            }catch (InputMismatchException ime){
-                System.out.println("Debes introducir un numero no una letra, vuelve a intentarlo.");
-                isNumber=false;
+        }while(decision>5);
 
-            }finally {
-                lector.nextLine();
-            }
-        }while(!isNumber);
+        System.out.println("Opcion:"+decision);
 
         return decision;
 
     }
-    public void datosAleatorios(){
+    public int datosAleatorios(){
+
         int[] vector = new int[20];
+        int numero;
+        int numAl=rnd.nextInt(vector.length);
+
+        for (int i = 0; i <vector.length ; i++) {
+            vector[i]=i;
+        }
+        numero=vector[numAl];
+
+        return numero;
+
     }
 }
